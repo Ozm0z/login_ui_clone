312380b552ede627273f43fd96a431e6bed7c791
Özgün Can Erdoğan <ozgungezi@gmail.com>
2024-10-29 06:32:33 +0300

    tema rengi mavi olarak değiştirildi
    tema rengi değiştirilirken hata fark edildi: klavye açılınca pixeller uyuşmuyor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Login Page',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
 
         // textTheme: TextTheme()
